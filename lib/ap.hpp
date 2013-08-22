eae854f697d8917131ff589553cf8992c039c177
William Millard <wmillard1@gmail.com>
2013-08-22 00:06:15 -0700

    Changed toolchain code and src tree structure

---
+++ lib/ap.hpp
@@ -0,0 +1,14 @@
+/***********************************************************************
+ * ap.hpp                                                              *
+ *                                                                     *
+ * Copyright 2013 William Patrick Millard <wmillard1@gmail.com>        *
+ *                                                                     *
+ * This file is an object designed to organize a single remote access  *
+ * point's info to be used for later use - namely:                     *
+ *   o. To organize individual access point information by object.     *
+ *   o. To use these individual listings to establish ssh sessions.    *
+ *   o. To keep a history of individual statistics and logs.           *
+ *                                                                     *
+ **********************************************************************/
+
+
