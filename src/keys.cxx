136d34250efd9eb1de9511d7a491baf9a206e090
William Millard <wmillard1@gmail.com>
2013-08-22 23:24:14 -0700

    FIXED BUILDING/LINKING and added template file for key management

---
+++ src/keys.cxx
@@ -0,0 +1,11 @@
+/***********************************************************************
+ * keys.cxx                                                            *
+ *                                                                     *
+ * Copyright 2013 William Patrick Millard <wmillard1@gmail.com>        *
+ *                                                                     *
+ * This file is an object designed to abstract key management into one *
+ * class (as used in the scope of this project, anyway).               *
+ *                                                                     *
+ **********************************************************************/
+
+
