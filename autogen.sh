4504d6e1c99d347e56beac0231bfdc6e55dba62e
William Millard <wmillard1@gmail.com>
2013-10-14 16:23:05 -0700

    Long story short: I broke all the things

--- autogen.sh
+++ autogen.sh
@@ -1,3 +1,6 @@
 #!/bin/sh
-autoreconf --install --force
-./configure
+# This file (script) automagically calls autotools for a rebuild
+
+autoreconf --install --force --verbose -I m4 $@ 
+
+./configure --prefix=''
